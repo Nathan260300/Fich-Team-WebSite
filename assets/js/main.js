f11852cc9a7a466421b2e1d181032429d74314c7
nathan260300 <nathan162318@gmail.com>
2026-04-02 22:05:39 +0200

    MAJ

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ console.log('%c© 2026 - FICH Team',           'background:#282c34;color:#98c379
 console.log('%cFICH Team',                     'background:#282c34;color:#61afef;padding:.5em 1em;border-radius:5px;font-weight:bold;');
 console.log('%cFICH Team : communauté de joueurs passionnés, force, intelligence, charisme et honneur.', 'background:#282c34;color:#61dafb;padding:.5em 1em;border-radius:5px;font-weight:bold;');
 console.log('%chttps://fich-team.netlify.app', 'background:#282c34;color:#e06c75;padding:.5em 1em;border-radius:5px;font-weight:bold;');
-console.log('%cMade with 🕑 and 💖 by Nathan The Coder – Last update : 28/03/2026', 'background:#282c34;color:#c678dd;padding:.5em 1em;border-radius:5px;font-weight:bold;');
+console.log('%cMade with 🕑 and 💖 by Nathan The Coder – Last update : 02/04/2026', 'background:#282c34;color:#c678dd;padding:.5em 1em;border-radius:5px;font-weight:bold;');
 
 document.getElementById('year').textContent = new Date().getFullYear();
 
