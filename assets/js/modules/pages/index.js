9e943b722d0480a240eaacac41b6f2a7be3b1ac5
nathan260300 <nathan162318@gmail.com>
2026-05-23 14:22:52 +0200

    MAJ

--- assets/js/modules/pages/index.js
+++
@@ -1,7 +0,0 @@
-export * as home     from './home.js';
-export * as partners from './partners.js';
-export * as projects from './projects.js';
-export * as join     from './join.js';
-
-import * as _404 from './404.js';
-export { _404 as page404 };
