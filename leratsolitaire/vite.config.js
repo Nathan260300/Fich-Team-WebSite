ce8bf8e2f9e14e2ccbdb6b1165a8a7f490431ac4
nathan260300 <nathan162318@gmail.com>
2026-05-23 14:28:38 +0200

    MAJ

--- leratsolitaire/vite.config.js
+++ leratsolitaire/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/leratsolitaire-wiki/',
   plugins: [react()],
+  build: {
+    outDir: '../dist/leratsolitaire'
+  }
 })
